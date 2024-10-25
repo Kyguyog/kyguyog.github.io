00ed2831702019067a6f174dc8ad9d81ae11a317
Kyguy2.0 <thaynekyan14@gmail.com>
2024-10-24 20:14:58 -0600

    Optimized Code

--- Snow Rider/www.hoodamath.com/newgames.js
+++
@@ -1,9 +0,0 @@
-var newGames = [
-"Hooda Math Escape Room Harrisburg",	
-"Duck Life",
-"Trade West",
-"Hooda Math Escape Room Dover",
-"Graphing Gala",
-"Hooda Math Escape Room Trenton",
-"Green Physics"
-];
